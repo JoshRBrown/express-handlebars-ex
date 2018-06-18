d541e4e138c89ad70b8b528e7104f13c5f27b29b
Josh Brown <jbrown016@gmail.com>
2018-06-18 11:18:39 -0400

    added routes for /contacts and /contacts/id

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const app = express();
 
 
 
+// Show the user a welcome message
 app.get('/', (req, res) => {
     res.send('Welcome to the machine');
 });
+
+
+// Contact list page. Show the user all contacts
+app.get('/contacts', (req, res) => {
+    res.send('You are on the listing page.')
+})
+// Contact detailed page. Show the user all info for one cantact.
+app.get('/contacts/:id', (req, res) => {
+    res.send(`You are viewing details for ${req.params.id}`);
+})
+
 
 app.listen(5000, () => {
     console.log('Theyre heeerrrrreeeee')
